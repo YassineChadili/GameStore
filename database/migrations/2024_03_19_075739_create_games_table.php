9d0126618329aed98fe22a2df643d75db6637002
YassineChadili <yassinechadili12@outlook.com>
2024-03-19 09:13:58 +0100

    Add migrations

--- database/migrations/2024_03_19_075739_create_games_table.php
+++ database/migrations/2024_03_19_075739_create_games_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('admin')->nullable();
-            $table->rememberToken();
+            $table->string('image');
+            $table->decimal('price');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('games');
     }
 };
--- database/migrations/2024_03_19_075925_create_game_consoles_table.php
+++ database/migrations/2024_03_19_075925_create_game_consoles_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('game_consoles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('admin')->nullable();
-            $table->rememberToken();
+            $table->foreignId('game_id')->references('id')->on('games');
+            $table->foreignId('console_id')->references('id')->on('consoles');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('game_consoles');
     }
 };
